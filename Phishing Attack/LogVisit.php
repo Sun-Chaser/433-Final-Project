9b8a0e61a10fe185f108c52fa552325a3a512721
Jack Yang <91767770+Sun-Chaser@users.noreply.github.com>
2025-04-22 12:39:49 -0500

    PHP

---
+++ Phishing Attack/LogVisit.php
@@ -0,0 +1,21 @@
+<?php
+
+    require 'database.php';
+
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+    $type = "visit";
+
+    $statement = $mysqli->prepare("insert into requests (ip_address, type) values (?, ?)");
+
+    if (!$statement) {
+        printf("Error Preparing Query: %s\n", $mysqli->error);
+        exit;
+    } else {
+        $statement->bind_param("ss", $ip_address, $type);
+        $statement->execute();
+        $statement->close();
+        printf("Query Sent Successfully!");
+        exit;
+    }
+
+?>
